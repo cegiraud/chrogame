d5eedef8b9579339da4a5a180e198be6f8ad8947
Charles <charleseric.giraud@gmail.com>
2014-02-16 20:48:38 +0100

    c'est good

--- chrogame/js/popup.js
+++ chrogame/js/popup.js
@@ -1,3 +1,29 @@
 /**
  * Created by Charles on 15/02/14.
  */
+var bg = chrome.extension.getBackgroundPage();
+
+function ResourcesController($scope, $http) {
+    $scope.resources = bg.ogameResources;
+
+    function refresh(){
+        $scope.resources = bg.ogameResources;
+        $scope.$apply();
+        (function() {setTimeout(refresh, 1000);})();
+    }
+    refresh();
+}
+
+
+//pour que la tableau s'affiche bien
+$(document).ready(function(){
+    var maxWidth = 0;
+    $('tr td:last-child').each(function(index){
+        if ($(this).width() > maxWidth) {
+            maxWidth = $(this).width();
+        }
+    });
+    $('tr td:last-child').each(function(index){$(this).width(maxWidth);});
+});
+
+
